66a7d60648a65491b556855a13e709a008a26d22
faridmo <jurispenale@gmail.com>
2019-01-02 23:39:53 +0000

    ajout de l'appli

---
+++ src/main/java/com/lacgaa/tdsi/ConnectionServlet.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lacgaa.tdsi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author USER
+ */
+@WebServlet(name = "ConnectionServlet", urlPatterns = {"/connection"})
+public class ConnectionServlet extends HttpServlet {
+
+    
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+            
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            String email = request.getParameter("email");
+            String password = request.getParameter("mdp");
+            
+            if(!Authentification.testBD(email, password)){
+                response.sendRedirect("index.html");
+            }else{
+                response.sendRedirect("welcome.html");
+                request.getSession().setAttribute("login", email);
+            }
+            
+            
+            
+            
+      
+      
+      
+    }
+    
+    
+}
